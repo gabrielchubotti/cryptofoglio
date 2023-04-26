adda6436e982ffdd03ffbe7ca65a2300baf8a848
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-04-25 22:30:19 -0300

    Working on Home + sample view

--- CryptoFoglio/View/Home.swift
+++ CryptoFoglio/View/Home.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct Home: View {
+    
+    // VARIABLES
+    @State var currentCoin: String = "BTC"
+    @Namespace var animation
+    
     var body: some View {
         VStack{
             HStack(spacing: 15) {
@@ -28,6 +33,39 @@ struct Home: View {
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    /// MARK: Create Segmented Control
+    @ViewBuilder
+    func CustomControl() -> some View {
+        let coins = ["BTC","ETH","BNB"]
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack(spacing: 15) {
+                ForEach(coins, id: \.self){ coin in
+                    Text(coin)
+                        .foregroundColor(currentCoin == coin ? .white : .gray)
+                        .padding(.vertical, 6)
+                        .padding(.horizontal, 10)
+                        .contentShape(Rectangle())
+                        .background{
+                            if currentCoin == coin {
+                                Rectangle()
+                                    .fill(Color("Tab"))
+                                    .matchedGeometryEffect(id: "SEGMENTEDTAB", in: animation)
+                            }
+                        }
+                        .onTapGesture {
+                            withAnimation{currentCoin = coin}
+                        }
+                }
+            }
+        }
+        .background{
+            RoundedRectangle(cornerRadius: 5, style: .continuous)
+                .stroke(Color.white.opacity(0.2), lineWidth: 1)
+        }
+        .padding(.vertical)
+        
     }
         
 }
