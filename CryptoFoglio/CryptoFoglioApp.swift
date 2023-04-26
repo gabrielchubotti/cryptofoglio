b79599af4e6d0d4607503c1e96fde130d33a5bd4
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-04-25 22:10:35 -0300

    Initial Commit

---
+++ CryptoFoglio/CryptoFoglioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoFoglioApp.swift
+//  CryptoFoglio
+//
+//  Created by Gabriel Chu Bottiglieri on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoFoglioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
